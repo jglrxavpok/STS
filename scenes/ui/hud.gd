9284c1fa9cfa003c6e8a1f148672b03f2b7831cd
jglrxavpok <jglrxavpok@gmail.com>
2025-05-10 21:47:24 +0200

    HUD UI v0

---
+++ scenes/ui/hud.gd
@@ -0,0 +1,19 @@
+extends Control
+
+@onready var player_huds: Array[PlayerHud] = [
+	$Players/Player1,
+	$Players/Player2,
+	$Players/Player3,
+	$Players/Player4,
+]
+
+func _ready() -> void:
+	for hud in player_huds:
+		hud.setup()
+
+func _process(delta: float) -> void:
+	# TODO: test only
+	for i in range(player_huds.size()):
+		player_huds[i].set_life_count(1 if i % 2 == 0 else 3)
+		player_huds[i].change_damage(10.0*i)
+	pass
---
+++ scenes/ui/player_hud.gd
@@ -0,0 +1,25 @@
+extends Control
+
+class_name PlayerHud
+
+@export var player_index: int
+
+@onready var hearts = [
+	$Lives/Life1,
+	$Lives/Life2,
+	$Lives/Life3,
+]
+
+func setup() -> void:
+	$PlayerNumber.text = str(player_index+1)
+
+func change_damage(new_damage: float) -> void:
+	$DamageIndicator.text = str(snapped(new_damage, 0.1)) + "%"
+	# TODO: play anim?
+	
+func set_life_count(life_count: int) -> void:
+	for i in range(hearts.size()):
+		if i <= life_count:
+			hearts[i].visible = true
+		else:
+			hearts[i].visible = false
