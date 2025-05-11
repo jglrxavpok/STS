949c6ce295d3cdfc6918af01e52ede3df687522a
Julien VERNAY <dev@jvernay.fr>
2025-05-11 12:45:31 +0200

    Added kenney anims

--- scenes/characters/kenney/kenney.gd
+++ scenes/characters/kenney/kenney.gd
@@ -1,1 +1,28 @@
 extends CharaBase
+
+
+var accum: float
+
+func _process(delta: float) -> void:
+	const NB_FRAMES = 2
+	const FRAMES_PER_SEC = 6
+	accum = fmod(accum + delta * FRAMES_PER_SEC, 1.0)
+	var idx_anim = int(accum * NB_FRAMES)
+
+	if velocity.x > 0:
+		$Sprite3D.flip_h = false
+	elif velocity.x < 0:
+		$Sprite3D.flip_h = true
+
+	if is_on_floor():
+		if velocity.x > 0:
+			$Sprite3D.frame = 7 + idx_anim
+		elif velocity.x < 0:
+			$Sprite3D.frame = 7 + idx_anim
+		else:
+			$Sprite3D.frame = 5
+	else:
+		if velocity.y > 0:
+			$Sprite3D.frame = 6
+		else:
+			pass # Keep last frame
